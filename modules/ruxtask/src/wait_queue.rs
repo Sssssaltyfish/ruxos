93defdc03fc38947956b0ed5f3ea3bb4edcb72b5
Sssssaltyfish <47731209+Sssssaltyfish@users.noreply.github.com>
2024-01-27 20:14:04 +0800

    Updated `WaitQueue` that it supports more operation
    1. Metadata
    2. Manual queue/dequeue
    3. Notify by predicate

--- modules/ruxtask/src/wait_queue.rs
+++ modules/ruxtask/src/wait_queue.rs
@@ -7,8 +7,8 @@
  *   See the Mulan PSL v2 for more details.
  */
 
-use alloc::collections::VecDeque;
 use alloc::sync::Arc;
+use alloc::{collections::VecDeque, vec::Vec};
 use spinlock::SpinRaw;
 
 use crate::{AxRunQueue, AxTaskRef, CurrentTask, RUN_QUEUE};
@@ -35,11 +35,13 @@ use crate::{AxRunQueue, AxTaskRef, CurrentTask, RUN_QUEUE};
 /// WQ.wait(); // block until `notify()` is called
 /// assert_eq!(VALUE.load(Ordering::Relaxed), 1);
 /// ```
-pub struct WaitQueue {
-    queue: SpinRaw<VecDeque<AxTaskRef>>, // we already disabled IRQs when lock the `RUN_QUEUE`
+pub struct WaitQueueWithMetadata<Meta> {
+    queue: SpinRaw<VecDeque<(AxTaskRef, Meta)>>, // we already disabled IRQs when lock the `RUN_QUEUE`
 }
 
-impl WaitQueue {
+pub type WaitQueue = WaitQueueWithMetadata<()>;
+
+impl<Meta> WaitQueueWithMetadata<Meta> {
     /// Creates an empty wait queue.
     pub const fn new() -> Self {
         Self {
@@ -61,7 +63,7 @@ impl WaitQueue {
             // wake up by timer (timeout).
             // `RUN_QUEUE` is not locked here, so disable IRQs.
             let _guard = kernel_guard::IrqSave::new();
-            self.queue.lock().retain(|t| !curr.ptr_eq(t));
+            self.queue.lock().retain(|(t, _)| !curr.ptr_eq(t));
             curr.set_in_wait_queue(false);
         }
         #[cfg(feature = "irq")]
@@ -73,58 +75,37 @@ impl WaitQueue {
 
     /// Blocks the current task and put it into the wait queue, until other task
     /// notifies it.
-    pub fn wait(&self) {
+    pub fn wait_meta(&self, meta: Meta) {
         RUN_QUEUE.lock().block_current(|task| {
             task.set_in_wait_queue(true);
-            self.queue.lock().push_back(task)
+            self.queue.lock().push_back((task, meta))
         });
-        self.cancel_events(crate::current());
-    }
-
-    /// Blocks the current task and put it into the wait queue, until the given
-    /// `condition` becomes true.
-    ///
-    /// Note that even other tasks notify this task, it will not wake up until
-    /// the condition becomes true.
-    pub fn wait_until<F>(&self, condition: F)
-    where
-        F: Fn() -> bool,
-    {
-        loop {
-            let mut rq = RUN_QUEUE.lock();
-            if condition() {
-                break;
-            }
-            rq.block_current(|task| {
-                task.set_in_wait_queue(true);
-                self.queue.lock().push_back(task);
-            });
-        }
         self.cancel_events(crate::current());
     }
 
     /// Blocks the current task and put it into the wait queue, until other tasks
     /// notify it, or the given duration has elapsed.
     #[cfg(feature = "irq")]
-    pub fn wait_timeout(&self, dur: core::time::Duration) -> bool {
+    pub fn wait_timeout_meta(&self, dur: core::time::Duration, meta: Meta) -> bool {
         let deadline = dur + ruxhal::time::current_time();
-        self.wait_timeout_absolutely(deadline)
+        self.wait_timeout_absolutely_meta(deadline, meta)
     }
+
     /// Blocks the current task and put it into the wait queue, until other tasks
     /// notify it, or the given deadling has elapsed.
-    pub fn wait_timeout_absolutely(&self, _deadline: core::time::Duration) -> bool {
+    pub fn wait_timeout_absolutely_meta(&self, deadline: core::time::Duration, meta: Meta) -> bool {
         let curr = crate::current();
         debug!(
             "task wait_timeout: {} deadline={:?}",
             curr.id_name(),
-            _deadline
+            deadline
         );
         #[cfg(feature = "irq")]
-        crate::timers::set_alarm_wakeup(_deadline, curr.clone());
+        crate::timers::set_alarm_wakeup(deadline, curr.clone());
 
         RUN_QUEUE.lock().block_current(|task| {
             task.set_in_wait_queue(true);
-            self.queue.lock().push_back(task)
+            self.queue.lock().push_back((task, meta))
         });
         let timeout = curr.in_wait_queue(); // still in the wait queue, must have timed out
         self.cancel_events(curr);
@@ -137,9 +118,14 @@ impl WaitQueue {
     /// Note that even other tasks notify this task, it will not wake up until
     /// the above conditions are met.
     #[cfg(feature = "irq")]
-    pub fn wait_timeout_until<F>(&self, dur: core::time::Duration, condition: F) -> bool
+    pub fn wait_timeout_until_meta<F>(
+        &self,
+        dur: core::time::Duration,
+        mut condition: F,
+        meta: Meta,
+    ) -> bool
     where
-        F: Fn() -> bool,
+        F: FnMut() -> bool,
     {
         let curr = crate::current();
         let deadline = ruxhal::time::current_time() + dur;
@@ -159,7 +145,7 @@ impl WaitQueue {
             }
             rq.block_current(|task| {
                 task.set_in_wait_queue(true);
-                self.queue.lock().push_back(task);
+                self.queue.lock().push_back((task, meta));
             });
         }
         self.cancel_events(curr);
@@ -186,7 +172,7 @@ impl WaitQueue {
     pub fn notify_all(&self, resched: bool) {
         loop {
             let mut rq = RUN_QUEUE.lock();
-            if let Some(task) = self.queue.lock().pop_front() {
+            if let Some((task, _)) = self.queue.lock().pop_front() {
                 task.set_in_wait_queue(false);
                 rq.unblock_task(task, resched);
             } else {
@@ -203,17 +189,42 @@ impl WaitQueue {
     pub fn notify_task(&self, resched: bool, task: &AxTaskRef) -> bool {
         let mut rq = RUN_QUEUE.lock();
         let mut wq = self.queue.lock();
-        if let Some(index) = wq.iter().position(|t| Arc::ptr_eq(t, task)) {
+        if let Some(index) = wq.iter().position(|(t, _)| Arc::ptr_eq(t, task)) {
             task.set_in_wait_queue(false);
-            rq.unblock_task(wq.remove(index).unwrap(), resched);
+            rq.unblock_task(wq.remove(index).unwrap().0, resched);
             true
         } else {
             false
         }
     }
 
+    /// Wake up all corresponding tasks that `filter` returns true.
+    ///
+    /// If `resched` is true, the current task will be preempted when the
+    /// preemption is enabled.
+    pub fn notify_task_if<F>(&self, resched: bool, mut filter: F) -> bool
+    where
+        F: FnMut(&AxTaskRef, &Meta) -> bool,
+    {
+        let mut rq = RUN_QUEUE.lock();
+        let mut wq = self.queue.lock();
+        let len_before = wq.len();
+
+        wq.retain(|(task, meta)| {
+            if filter(task, meta) {
+                task.set_in_wait_queue(false);
+                rq.unblock_task(task.clone(), resched);
+                false
+            } else {
+                true
+            }
+        });
+
+        len_before != wq.len()
+    }
+
     pub(crate) fn notify_one_locked(&self, resched: bool, rq: &mut AxRunQueue) -> bool {
-        if let Some(task) = self.queue.lock().pop_front() {
+        if let Some((task, _)) = self.queue.lock().pop_front() {
             task.set_in_wait_queue(false);
             rq.unblock_task(task, resched);
             true
@@ -223,9 +234,161 @@ impl WaitQueue {
     }
 
     pub(crate) fn notify_all_locked(&self, resched: bool, rq: &mut AxRunQueue) {
-        while let Some(task) = self.queue.lock().pop_front() {
+        while let Some((task, _)) = self.queue.lock().pop_front() {
             task.set_in_wait_queue(false);
             rq.unblock_task(task, resched);
         }
+    }
+
+    /// Queue a given task with its metadata given.
+    ///
+    /// It is marked as unsafe as it does nothing other than queueing the task,
+    /// so one might want to modify the state of the task appropriately before/after preforming
+    /// this operation.
+    pub unsafe fn queue_task_meta(&self, task: AxTaskRef, meta: Meta) {
+        self.queue.lock().push_back((task, meta));
+    }
+
+    /// Dequeue the given task, returning its metadata if this task is actually inside this queue.
+    ///
+    /// It is marked as unsafe as it does nothing other than dequeueing the task,
+    /// so one might want to modify the state of the task appropriately before/after preforming
+    /// this operation.
+    pub unsafe fn dequeue_task(&self, task: &AxTaskRef) -> Option<Meta> {
+        let mut wq = self.queue.lock();
+        let pos = wq.iter().position(|(t, _)| Arc::ptr_eq(t, task));
+        if let Some(pos) = pos {
+            let (_, meta) = wq.swap_remove_back(pos).unwrap();
+            Some(meta)
+        } else {
+            None
+        }
+    }
+
+    /// Dequeue all corresponding tasks that `filter` returns true.
+    ///
+    /// It is marked as unsafe as it does nothing other than dequeueing the tasks,
+    /// so one might want to modify the state of the task appropriately before/after preforming
+    /// this operation.
+    pub unsafe fn dequeue_tasks_if<F, Op>(&self, mut filter: F, mut dequeue_op: Op)
+    where
+        F: FnMut(&AxTaskRef, &Meta) -> bool,
+        Op: FnMut(AxTaskRef, Meta),
+    {
+        let mut wq = self.queue.lock();
+        let first_false = partition_deque(&mut wq, |(t, m)| filter(t, m));
+
+        // `..first_false` represents the range that `filter` returns true for all tasks within.
+        wq.drain(..first_false).for_each(|(t, m)| dequeue_op(t, m));
+    }
+}
+
+impl<Meta: Clone> WaitQueueWithMetadata<Meta> {
+    /// Blocks the current task and put it into the wait queue, until the given
+    /// `condition` becomes true.
+    ///
+    /// Note that even other tasks notify this task, it will not wake up until
+    /// the condition becomes true.
+    pub fn wait_until_meta<F>(&self, mut condition: F, meta: Meta)
+    where
+        F: FnMut() -> bool,
+    {
+        loop {
+            let mut rq = RUN_QUEUE.lock();
+            if condition() {
+                break;
+            }
+            rq.block_current(|task| {
+                task.set_in_wait_queue(true);
+                self.queue.lock().push_back((task, meta.clone()));
+            });
+        }
+        self.cancel_events(crate::current());
+    }
+}
+
+impl<Meta: Default> WaitQueueWithMetadata<Meta> {
+    /// Blocks the current task and put it into the wait queue, until other task
+    /// notifies it.
+    pub fn wait(&self) {
+        self.wait_meta(Default::default())
+    }
+
+    /// Blocks the current task and put it into the wait queue, until other tasks
+    /// notify it, or the given duration has elapsed.
+    #[cfg(feature = "irq")]
+    pub fn wait_timeout(&self, dur: core::time::Duration) -> bool {
+        self.wait_timeout_meta(dur, Default::default())
+    }
+
+    /// Blocks the current task and put it into the wait queue, until other tasks
+    /// notify it, or the given deadling has elapsed.
+    pub fn wait_timeout_absolutely(&self, deadline: core::time::Duration) -> bool {
+        self.wait_timeout_absolutely_meta(deadline, Default::default())
+    }
+
+    /// Blocks the current task and put it into the wait queue, until the given
+    /// `condition` becomes true, or the given duration has elapsed.
+    ///
+    /// Note that even other tasks notify this task, it will not wake up until
+    /// the above conditions are met.
+    #[cfg(feature = "irq")]
+    pub fn wait_timeout_until<F>(&self, dur: core::time::Duration, condition: F) -> bool
+    where
+        F: FnMut() -> bool,
+    {
+        self.wait_timeout_until_meta(dur, condition, Default::default())
+    }
+
+    /// Queue a given task with "default" metadata.
+    ///
+    /// It is marked as unsafe as it does nothing other than queueing the task,
+    /// so one might want to modify the state of the task appropriately before/after preforming
+    /// this operation.
+    pub unsafe fn queue_task(&self, task: AxTaskRef) {
+        self.queue.lock().push_back((task, Default::default()));
+    }
+}
+
+impl<Meta: Default + Clone> WaitQueueWithMetadata<Meta> {
+    /// Blocks the current task and put it into the wait queue, until the given
+    /// `condition` becomes true.
+    ///
+    /// Note that even other tasks notify this task, it will not wake up until
+    /// the condition becomes true.
+    pub fn wait_until<F>(&self, condition: F)
+    where
+        F: FnMut() -> bool,
+    {
+        self.wait_until_meta(condition, Default::default())
+    }
+}
+
+/// Partition a [`VecDeque`] in-place so that it contains all elements for
+/// which `predicate(e)` is `true`, followed by all elements for which
+/// `predicate(e)` is `false`.
+/// Returns the index of the first element which returned false.
+/// Returns 0 if all elements returned false.
+/// Returns `data.len()` if all elements returned true.
+fn partition_deque<T, F>(data: &mut VecDeque<T>, mut pred: F) -> usize
+where
+    F: FnMut(&T) -> bool,
+{
+    let len = data.len();
+    if len == 0 {
+        return 0;
+    }
+    let (mut l, mut r) = (0, len - 1);
+    loop {
+        while l < len && pred(&data[l]) {
+            l += 1;
+        }
+        while r > 0 && !pred(&data[r]) {
+            r -= 1;
+        }
+        if l >= r {
+            return l;
+        }
+        data.swap(l, r);
     }
 }
